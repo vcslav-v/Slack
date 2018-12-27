83cf3b2d7901d697caaac3ca5e0f2d22677729a7
nuswen <donn.nus@gmail.com>
2018-12-27 12:24:25 +0300

    d

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from os import environ
+import os
 from slackclient import SlackClient
 from flask import Flask, request
 #from flask_sqlalchemy import SQLAlchemy
 
-sc = SlackClient(environ('TOKEN'))
+sc = SlackClient(os.environ('TOKEN'))
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL']
 #db = SQLAlchemy(app)
