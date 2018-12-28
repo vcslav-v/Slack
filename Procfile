244368a3cbeea9d06638786a249af0565e4eab3e
nuswen <donn.nus@gmail.com>
2018-12-28 09:54:25 +0300

    init

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python run.py
+web: gunicorn slack_app_test:app
--- slack_app_test.py
+++ slack_app_test.py
@@ -1,5 +1,3 @@
-#from app import app
-#app.run()
 import json
 import os
 import requests
@@ -7,6 +5,12 @@ from pprint import pprint as pp
 
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 SLACK_WEBHOOK_INC = os.environ.get('SLACK_WEBHOOK_INC')
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def on_root():
+    return make_response('<h1>Hello world!</h1>', 200)
 
 def slack_post_msg(text, channel, **kwargs):
     data = {
@@ -27,9 +31,12 @@ def slack_post_msg(text, channel, **kwargs):
             json.dumps(json.loads(response.text), indent=4)
     ))
 
-slack_post_msg(
-    text='Hello from epic bot!',
-    channel='testch',
-    #channel='#random',
-    icon_emoji=':sunglasses:'
-    )
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=8080, threaded=True)
+# slack_post_msg(
+#     text='Hello from epic bot!',
+#     channel='testch',
+#     #channel='#random',
+#     icon_emoji=':sunglasses:'
+#     )
