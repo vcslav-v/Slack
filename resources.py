843e1272ef873269d81d015d417fdd3b1d389860
nuswen <donn.nus@gmail.com>
2018-12-28 20:45:07 +0300

    init

--- resources.py
+++ resources.py
@@ -1,60 +1,61 @@
+import os
 dialog_income = {
-    "title": "Доход",
-    "submit_label": "Submit",
-    "callback_id": "income_form",
-    "elements": [
+    'title': 'Доход',
+    'submit_label': 'Submit',
+    'callback_id': 'income_form',
+    'elements': [
         {
-            "label": "Категория",
-            "type": "select",
-            "name": "income_from",
-            "placeholder": "Выберите категорию",
-            "value": "products",
-            "options": [
+            'label': 'Категория',
+            'type': 'select',
+            'name': 'income_from',
+            'placeholder': 'Выберите категорию',
+            'value': 'products',
+            'options': [
                 {
-                    "label": "Products",
-                    "value": "products"
+                    'label': 'Products',
+                    'value': 'products'
                 },
                 {
-                    "label": "Email",
-                    "value": "email"
+                    'label': 'Email',
+                    'value': 'email'
                 },
                 {
-                    "label": "Plus",
-                    "value": "plus"
+                    'label': 'Plus',
+                    'value': 'plus'
                 },
                 {
-                    "label": "Banners",
-                    "value": "banners"
+                    'label': 'Banners',
+                    'value': 'banners'
                 }
             ]
         },
 
         {
-            "label": "Валюта",
-            "type": "select",
-            "name": "income_currency",
-            "placeholder": "Выберите валюту",
-            "value": "usd",
-            "options": [
+            'label': 'Валюта',
+            'type': 'select',
+            'name': 'income_currency',
+            'placeholder': 'Выберите валюту',
+            'value': 'usd',
+            'options': [
                 {
-                    "label": "USD",
-                    "value": "usd"
+                    'label': 'USD',
+                    'value': 'usd'
                 },
                 {
-                    "label": "RUR",
-                    "value": "rur"
+                    'label': 'RUR',
+                    'value': 'rur'
                 },
                 {
-                    "label": "EUR",
-                    "value": "eur"
+                    'label': 'EUR',
+                    'value': 'eur'
                 }
             ]
         },
 
         {
-            "type": "text",
-            "label": "Сколько?",
-            "name": "income_value"
+            'type': 'text',
+            'label': 'Сколько?',
+            'name': 'income_value'
         }
     ]
 }
@@ -72,4 +73,17 @@ mouth_dic = {
     '10':'k',
     '11':'l',
     '12':'m'
+}
+
+client_secret = {
+    'type': , os.environ.get('g_type'),
+    'project_id': os.environ.get('g_project_id'),
+    'private_key_id': os.environ.get('g_private_key_id'),
+    'private_key': os.environ.get('g_private_key),
+    'client_email': os.environ.get('g_client_email'),
+    'client_id': os.environ.get('g_client_id'),
+    'auth_uri': os.environ.get('g_auth_uri'),
+    'token_uri': os.environ.get('g_token_uri'),
+    'auth_provider_x509_cert_url': os.environ.get('g_auth_provider_x509_cert_url'),
+    'client_x509_cert_url': os.environ.get('g_client_x509_cert_url')
 }
