7e77f31b471e2d47277f0273fd1497fb608a816b
nuswen <donn.nus@gmail.com>
2019-01-08 13:36:51 +0300

    init

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python run.py
+web: gunicorn fin_bot:app
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import db
+from fin_bot import db
 
 class finam_income(db.Model):
     id = db.Column(db.Integer, primary_key=True)
