414a16ebedd795c533e8976c6eefd9239fe2aba0
nuswen <donn.nus@gmail.com>
2020-05-11 19:39:34 +0300

    init

--- fin_bot.py
+++ fin_bot.py
@@ -94,7 +94,7 @@ def tocash():
 # Обрабатываем форму
 @app.route('/api/interactive_action', methods=['POST'])
 def on_interactive_action():
-    pp(flask.request.data)
+    pp(flask.request.json)
     """
     response_text = ''
     interactive_action = json.loads(flask.request.values['payload'])
